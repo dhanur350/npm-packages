17011dcbdba0ba5ec10ca0d38066627972886a80
Sahil Rajput <sahilrajput03@gmail.com>
2023-10-22 23:20:57 +0530

    --no-edit

--- react-table-scrollable/examples-development/example1-cra/src/index.js
+++ react-table-scrollable/examples-development/example1-cra/src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ReactTable from "@dhanur350/react-table-scrollable"
-import { managementTableHeaders, tableData } from './data'
-
-const MyTable = () => {
-  return <ReactTable
-    isScrollable={true}
-    tableHeaders={managementTableHeaders}
-    tableData={tableData}
-    tableHeight={"20rem"}
-  />
-}
+import MyTable from './MyTable'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
