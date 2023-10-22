48ddd8720031dfcfd7b7b3e59f374d4369f5f694
Sahil Rajput <sahilrajput03@gmail.com>
2023-10-22 22:10:01 +0530

    Fix exampel1-vite

---
+++ react-table-scrollable/examples/example1-vite/src/MyTable.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import ReactTable, { Counter } from "@dhanur350/react-table-scrollable"
+import { managementTableHeaders, tableData } from './data'
+
+const MyTable = () => {
+	return <div>
+
+		<ReactTable
+			isScrollable={true}
+			tableHeaders={managementTableHeaders}
+			tableData={tableData}
+			tableHeight={"20rem"}
+		/>
+
+		<Counter />
+	</div>
+}
+export default MyTable
